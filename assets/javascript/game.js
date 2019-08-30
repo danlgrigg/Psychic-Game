03a6806f67926b03988c3ca43ae583229808a572
danoprotagonist <griggdl@gmail.com>
2019-08-30 02:04:26 -0700

    CSS: Added background image; restyled text.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,22 +3,22 @@
 var computerChoiceArr = [
 "a", "b", "c","d", "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
 ];
- //start game with scores for each variable
+
+ //start game with scores for each variable as a counter
 
 var wins = 0
 var losses = 0
 var guessesLeft = 9
 var guessesSoFar =[];
+
 // var userGuess = document.onkeyup;
 
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guesses-left");
 var guessesSoFarText = document.getElementById("guesses-so-far");
+
 //create count variable
-
-
-
 
 var computerGuess = computerChoiceArr[Math.floor(Math.random()* computerChoiceArr.length)];
 console.log(computerGuess);
@@ -29,15 +29,12 @@ document.onkeyup = function(event) {
     var userGuess = event.key;
     console.log(userGuess);
     guessesSoFar.push(userGuess);
-         
-
-    
-    
-    // //compare user guess to computer pick
+   
+    //compare user guess to computer pick
     //create a counter to track wins, losses, guesses left, guesses so far
     
     if (userGuess === computerGuess){
-        wins++;
+        wins++;  
         alert("You really ARE psychic!!! I was thinking of the letter " + (computerGuess) + ".");
         guessesSoFar=[];        
         guessesLeft=9;
